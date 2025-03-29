0c9af47850a97ce288e6a238f1c9bec4db03bfd1
texotek <fdahmen@proton.me>
2025-03-29 17:08:55 +0100

    feat(testing): change baseurl to point at localhost:8081

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
 describe('Availability Testing', () => {
   it('Opens the shopping list Application', () => {
-    cy.visit('http://localhost:8081')
+    cy.visit('/')
 
     cy.contains('Shopping Lists')
   })
