dd731393c20c44e2eaabd890677cf5da01fa7505
texotek <texotek@users.noreply.github.com>
2025-04-10 09:24:38 +0000

    Deploying to gh-pages from @ TGM-HIT/syt5-gek1051-mobile-application-fmms_shoppinglist@bc7f4b5c135ce586a70677577ea67b47f95d0988 🚀

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/syt5-gek1051-mobile-application-fmms_shoppinglist/sw.js', { scope: '/syt5-gek1051-mobile-application-fmms_shoppinglist/' })})}
